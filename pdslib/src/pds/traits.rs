2ae6129e839ed369a75933504ca5899cf6cbc83d
Alison Caulfield <alisoncaulfield@alisons-mbp.mynetworksettings.com>
2024-10-28 12:37:23 -0400

    Removing nested trait bounds for PrivateDataService.compute_report

--- pdslib/src/pds/traits.rs
+++ pdslib/src/pds/traits.rs
@@ -2,13 +2,11 @@ use crate::queries::traits::ReportRequest;
 
 pub trait PrivateDataService {
     type Event;
-    type EpochId;
-    type EpochEvents;
-    // type ReportRequest;
-    // type Report; // TODO: is this going to be a union type over possible reports? Sounds pretty resonable. Or trait with dynamic dispatch?
+    type ReportRequest;
+    type Report; // TODO: is this going to be a union type over possible reports? Sounds pretty resonable. Or trait with dynamic dispatch?
 
     fn register_event(&mut self, event: Self::Event) -> Result<(), ()>;
 
     // TODO: where do we restrict the list of supported query types? Maybe allow them all to run for now, an return null reports if not supported?
-    fn compute_report<R: ReportRequest<EpochId = Self::EpochId, EpochEvents = Self::EpochEvents>>(&mut self, request: R) -> R::Report;
+    fn compute_report(&mut self, request: Self:: ReportRequest) -> Self::Report;
 }

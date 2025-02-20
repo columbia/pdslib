5f83e4cb00eae1b18d5428cd547a9127cb4aa626
Benjamin M. Case <35273659+bmcase@users.noreply.github.com>
2025-02-20 16:45:55 -0500

    update in comments
    update in comments about how we're only considering one querier in scope for the demo

--- tests/ppa_workflow.rs
+++ tests/ppa_workflow.rs
@@ -12,7 +12,8 @@ use pdslib::{
 
 #[test]
 fn main() {
-    // This demo represents what happens on a single device.
+    // This demo represents what happens on a single device and
+    // for managing the budget of a single querier
     // Scenario similar to https://arxiv.org/pdf/2405.16719, Section 3.3
 
     // Set up storage and Private Data Service.

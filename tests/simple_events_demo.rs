43b77b0c27d2d40055f0f25e0078c83f347a617f
mardev unit bcdev <mychen0912@gmail.com>
2025-03-09 22:16:22 -0400

    Added data structure changes to enable q-source filters.

--- tests/simple_events_demo.rs
+++ tests/simple_events_demo.rs
@@ -25,6 +25,7 @@ fn main() -> Result<(), anyhow::Error> {
         PureDPBudget::Epsilon(3.0),
         PureDPBudget::Epsilon(20.0),
         PureDPBudget::Epsilon(3.5),
+        PureDPBudget::Epsilon(3.5),
     );
     let filters: HashMapFilterStorage<_, PureDPBudgetFilter, _, _> =
         HashMapFilterStorage::new(capacities)?;

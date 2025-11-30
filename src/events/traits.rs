4de3757828a35efccabb83bb82c0f4b45035c9d9
Giorgio <20621396+giorgi-o@users.noreply.github.com>
2025-11-29 22:58:31 -0500

    improve EventStorage::relevant_events_for_epoch() documentation

--- src/events/traits.rs
+++ src/events/traits.rs
@@ -5,7 +5,6 @@ use std::{
 
 use crate::events::uri_set::UriSet;
 
-
 /// Marker trait with bounds for epoch identifiers.
 pub trait EpochId: Clone + Copy + Debug + Eq + Hash {}
 
@@ -75,14 +74,16 @@ pub trait EventStorage {
 
     /// Retrieves relevant events for a specific epoch, filtered by the
     /// provided selector.
-    ///
-    /// A default implementation exists, however a more efficient one
-    /// can be provided if allowed by the storage implementation.
     fn relevant_events_for_epoch(
         &mut self,
         epoch_id: &<Self::Event as Event>::EpochId,
         relevant_event_selector: &impl RelevantEventSelector<Event = Self::Event>,
     ) -> Result<impl Iterator<Item = Self::Event>, Self::Error> {
+        // This is the default implementation. It can be inefficient because
+        // it retrieves (and potentially clones) ALL events for the epoch
+        // before filtering out the irrelevant ones.
+        // Storage implementations should override this method if they can
+        // perform the filtering more efficiently (e.g. before allocation).
         let iter = self
             .events_for_epoch(epoch_id)?
             .filter(|event| relevant_event_selector.is_relevant_event(event));

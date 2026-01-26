6e9e056134df447a8cc64f1e903922c2173144c5
Jessica Card <jessicard@mac.com>
2026-01-26 15:27:49 -0500

    Remove unused getters

--- bindings/python/src/filter_id.rs
+++ bindings/python/src/filter_id.rs
@@ -8,29 +8,6 @@ pub struct PyFilterId {
     pub inner: FilterId<u64, String>,
 }
 
-#[pymethods]
-impl PyFilterId {
-    #[getter]
-    fn filter_type(&self) -> &'static str {
-        match &self.inner {
-            FilterId::PerQuerier(_, _) => "PerQuerier",
-            FilterId::Global(_) => "Global",
-            FilterId::TriggerQuota(_, _) => "TriggerQuota",
-            FilterId::SourceQuota(_, _) => "SourceQuota",
-        }
-    }
-
-    #[getter]
-    fn epoch_id(&self) -> u64 {
-        *self.inner.epoch_id()
-    }
-
-    #[getter]
-    fn uri(&self) -> Option<String> {
-        self.inner.uri().cloned()
-    }
-}
-
 impl From<FilterId<u64, String>> for PyFilterId {
     fn from(inner: FilterId<u64, String>) -> Self {
         PyFilterId { inner }
